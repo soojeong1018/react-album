bbbed55b5f3cc35f9eceb60a5abf82e50465486d
ksj <tnwjd940412@gmail.com>
2024-09-09 00:24:41 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import MainPage from "./pages/index"
+import AboutPage from "./pages/about";
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index path="/" element={<MainPage />}></Route>
+                <Route index path="/about" element={<AboutPage />}></Route>
+                <Route index path="/about/:id" element={<AboutPage />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+function AboutPage() {
+    return (
+        <div>about page</div>
+    );
+}
+
+export default AboutPage;
